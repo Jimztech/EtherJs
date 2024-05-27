7f7e00f96e53747fbce56c9b38eca03fb052d281
toxxicjames <ibezim.dev@gmail.com>
2024-05-27 02:57:44 +0200

    updated proof verification

--- MerkleTree/verifyProof.js
+++ MerkleTree/verifyProof.js
@@ -1,8 +1,47 @@
 function verifyProof(proof, node, root, concat) {
+    let node = this.leaves;
+    if(node.length === 1) return proof;
     const sum = [];
-    for(let i = 0; i < proof.length; i++){
+    for(let i = 0; i < proof.length; i+=2){
         let left = node[i];
         let right = node[i + 1]; 
-        sum.push(concat(left, right));
+        // sum.push(concat(left, right));
+
+        if (!right) {
+            sum.push(left);
+        }
+        else {
+            sum.push(this.concat(left, right));
+
+            if (i === index || i === index - 1) {
+                let isLeft = !(index % 2);
+                proof.push({
+                    data: isLeft ? right : left,
+                    left: !isLeft
+                });
+            }
+        }
     }
+
+    const oldProof = getProof();
+    if(sum === oldProof){
+        console.log(true)
+    } else {
+        console.log(false);
+    }
+}
+
+
+// Correct or original one
+function verifyProof(proof, node, root, concat) {
+    let data = node;
+    for (let i = 0; i < proof.length; i++) {
+        if (proof[i].left) {
+            data = concat(proof[i].data, data);
+        }
+        else {
+            data = concat(data, proof[i].data);
+        }
+    }
+    return data === root;
 }
